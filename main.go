a3e1824d7d3f2eb56d83e9c64a8b169ad5698b41
joykingoo <wendokingoo@gmail.com>
2023-07-04 22:15:36 +0300

    main go file modified with port 8080 /localhost

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+)
+
+//Packages needed
 func main() {
+	//setting up a http request for you plus register it
+	http.HandleFunc("/you", func(w http.ResponseWriter, r *http.Request) {
 
-}
+		// when request is made below message is printed
+		fmt.Fprintf(w, "Hello person")
 
-func Listen(network, address string) (Listener, error)
+	})
+	//printout of server with time shown
+	log.Println("Starting server..")
 
-listener,error := net.Listen("tcp","8080")
-if error != nil {
-	//handle error
+	//server created at the localhost port usimg tcp
+	listener, err := net.Listen("tcp", ":8080")
+
+	//recieve error record and exit
+	if err != nil {
+		log.Fatal(err)
+	}
+	// setup HTTP connection for the listener of the server
+	http.Serve(listener, nil)
 }
